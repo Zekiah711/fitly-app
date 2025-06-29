2bb5df9586fd2b9e46294f942a0b4a9093fa79fc
Adebayo Temiloluwa <temiloluwaa980@example.com>
2025-06-29 09:36:08 +0100

    t

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Dashboard from './Dashboard';
+import Dashboard from './dashboard';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
